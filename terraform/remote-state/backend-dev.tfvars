65643fdaf0ee1c29370a629dfdbd27a5e9ba7ed1
burak <alamin.root@gmail.com>
2025-02-05 17:50:43 +0600

    add backends in remote-state

---
+++ terraform/remote-state/backend-dev.tfvars
@@ -0,0 +1,2 @@
+bucket = "proxyserver2023-sandbox-terraform-state"
+key    = "src/terraform/dev"
--- terraform/remote-state/providers.tf
+++ terraform/remote-state/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  backend "s3" {
+    region = var.region
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.84"
+    }
+  }
+}
+
+
+
 provider "aws" {
   region  = var.region
   profile = var.aws_profile
