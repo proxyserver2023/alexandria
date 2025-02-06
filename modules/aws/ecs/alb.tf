963076b62f34f29077cda2596dad0b805d74e261
burak <alamin.root@gmail.com>
2025-02-06 20:25:20 +0600

    Terraform Modules

--- modules/aws/ecs/alb.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lb" "my_alb" {
-  name               = "ecs-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = aws_subnet.public[*].id
-}
