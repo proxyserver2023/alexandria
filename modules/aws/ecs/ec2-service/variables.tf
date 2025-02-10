6b46dd92c7f764e40b8cfd7b6a9376fe5e63f2b4
burak <alamin.root@gmail.com>
2025-02-10 21:46:26 +0600

    removed unnecessary vars

--- modules/aws/ecs/ec2-service/variables.tf
+++ modules/aws/ecs/ec2-service/variables.tf
@@ -22,12 +22,6 @@ variable "ondemand_weight" {
 variable "desired_count" {
   type = number
 }
-variable "subnets" {
-  type = list(string)
-}
-variable "security_groups" {
-  type = list(string)
-}
 variable "target_group_arn" {
   type = string
 }
