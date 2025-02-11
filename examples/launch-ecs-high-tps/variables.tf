5b6126d57d0983b25243509e052b6e1e60f08c51
burak <alamin.root@gmail.com>
2025-02-11 11:14:31 +0600

    Final Touch before test

--- examples/launch-ecs-high-tps/variables.tf
+++ examples/launch-ecs-high-tps/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "name" {
+  description = "Project Name"
+  type        = string
+  default     = "alexandria"
+}
